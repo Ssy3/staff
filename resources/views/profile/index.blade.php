9ff0280f424451ab6a2dce5b7ede8a9a9b1732b9
SumayahAlharbi <ftooo-ff@hotmail.com>
2020-07-27 23:14:59 +0300

    Code cleaning

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,38 +7,6 @@
     <div class="card-header">
       My Profile
     </div>
-  {{--<div class="col-lg-4 col-xlg-3 col-md-5">
-      <div class="card">
-        <div class="card-body">
-
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-body">
-
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-8 col-xlg-9 col-md-7">
-      <div class="card">
-        <!-- Nav tabs -->
-        <ul class="nav nav-tabs profile-tab" role="tablist">
-          <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#userAttendance" role="tab">Attendance</a> </li>
-        </ul>
-        <!-- Tab panes -->
-        <div class="tab-content">
-          <div class="tab-pane active" id="userAttendance" role="tabpanel">
-            <div class="card-body">
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
---}}
-
 
     <table class="table table-striped">
       <thead>
